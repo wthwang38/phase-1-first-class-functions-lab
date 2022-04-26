9100c987563d92a4e824ad6801ed2bdac85d5327
William Hwang <willhwang90@gmail.com>
2022-04-26 17:31:48 -0400

    commit tree 4a5a04758726938390eff5c5cac566fb81be6238

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = () => drivers.slice(0,2);
+
+const returnLastTwoDrivers = () => drivers.slice(2);
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplying) => function(fare){
+return multiplying*fare;
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, whoTopick){
+    return whoTopick();
+}
